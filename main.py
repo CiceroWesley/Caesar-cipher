bbcda96f36ab6603bc302339ada3c4cc0a583d28
Lsuyane <liliasuyane2001@gmail.com>
2022-09-08 15:33:11 -0300

    Decrypt
    Função decifrar feita.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 
-
 alfabet ='abcdefghijklmnopqrstuvwxyz'
 
 def criptografar(text, k):
@@ -25,6 +24,28 @@ def criptografar(text, k):
         
     return ciferText    
 
+def descriptografar(text, k):
+    ciferText = ''
+    #para texto com mais de uma palavra
+    if  " " in text:
+        text = text.split(" ")
+        for i in range(len(text)):
+            ciferTextAux = ''
+            for letter in text[i]:
+                p = alfabet.index(letter)
+                newIndex = (p - k) % 26
+                ciferTextAux += alfabet[newIndex]
+            ciferTextAux += " "
+            ciferText +=ciferTextAux
+    #para texto com uma palavra
+    else:
+        for letter in text:
+            p = alfabet.index(letter)
+            newIndex = (p - k) % 26
+            ciferText += alfabet[newIndex]    
+        
+    return ciferText  
+
 def main():
     option = int(input("1 - Criptografar texto com chave k\n2 - Descriptografar texto com chave k\n3 - Descriptografar texto com força bruta\n"))
 
@@ -33,7 +54,9 @@ def main():
         k = int(input("Insira o k:"))
         print(criptografar(text,k))
     elif option == 2:
-        a = 1
+        text1 = input("Insira o texto:")
+        k = int(input("Insira o k:"))
+        print(descriptografar(text1,k))
     elif option == 3:
         a = 1
     else:
